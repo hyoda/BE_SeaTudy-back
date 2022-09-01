df7af221543f50b63e8ec6ceb4fc0c3cf0e9e11c
Heylim Kim <hlim9022@gmail.com>
2022-09-01 23:29:18 +0900

    feat: 카카오 로그인 구현완료

---
+++ src/main/java/com/finalproject/seatudy/login/LoginType.java
@@ -0,0 +1,8 @@
+package com.finalproject.seatudy.login;
+
+public enum LoginType {
+    GENERAL,
+    KAKAO,
+    NAVER,
+    GOOGLE
+}
---
+++ src/main/java/com/finalproject/seatudy/security/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.finalproject.seatudy.security.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public enum ErrorCode {
+    INVALID_AUTH_TOKEN(HttpStatus.BAD_REQUEST, "유효하지 않은 토큰입니다."),
+    AUTH_TOKEN_NOT_FOUND(HttpStatus.BAD_REQUEST, "토큰이 존재하지 않습니다."),
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 유저 정보를 찾을 수 없습니다.");
+
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
