7703c4a7c197236fa0c30791fda7c8ec85711007
Heylim Kim <hlim9022@gmail.com>
2022-09-11 01:08:04 +0900

    ci: CI/CD 추가 세팅 #33
    현재는 push 할 때마다 빌드 후 자동배포가 진행되도록 세팅

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app"
+PROJECT_ROOT="/home/ubuntu/SeaTUDY"
 JAR_FILE="$PROJECT_ROOT/SeaTUDY-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
