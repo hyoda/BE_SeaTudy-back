a0d80a23e81749169fcf22d0dd088310ffe69942
Heylim Kim <hlim9022@gmail.com>
2022-09-15 16:54:41 +0900

    build: 프론트 배포 후 configuration 수정

--- src/main/java/com/finalproject/seatudy/security/config/WebSocketConfig.java
+++ src/main/java/com/finalproject/seatudy/security/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.finalproject.seatudy.security.config;
 
 import com.finalproject.seatudy.security.StompHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -16,12 +17,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final StompHandler stompHandler;
 
+    @Value("${front.base.url}")
+    private String FRONT_BASE_URL;
+
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // SockJS Client or WebSocket이 HandShake Connection을 생성할 경로
         registry.addEndpoint("/api/v1/chat/connections")
-                .setAllowedOrigins("http://localhost:8080", "http://localhost:3000")
+                .setAllowedOrigins("http://localhost:8080", "http://localhost:3000",
+                        FRONT_BASE_URL)
                 .withSockJS();
     }
 
